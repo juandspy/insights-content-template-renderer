5d4919e1ec7e1b734c7643b930a796f1a13e4daf
Jiří Papoušek <jiri.pap@gmail.com>
2021-11-02 10:03:03 +0100

    Break down the utils module into more simple functions

--- utils.py
+++ utils.py
@@ -19,6 +19,53 @@ DoT_settings = {
     "selfcontained": False
 }
 
+
+def get_reported_module(report):
+    return report['rule_id'].split('|')[0]
+
+
+def get_reported_error_key(report):
+    return report['rule_id'].split('|')[1]
+
+
+def get_template_function(template_name, rule_content, report):
+    template_text = rule_content[template_name]
+
+    reported_error_key = get_reported_error_key(report)
+    error_key_content = rule_content['error_keys'][reported_error_key]
+
+    if template_name in error_key_content:
+        template_text = error_key_content[template_name]
+
+    return js2py.eval_js(DoT.template(template_text, DoT_settings))
+
+
+def render_resolution(rule_content, report):
+    resolution_template = get_template_function('resolution', rule_content, report)
+    return resolution_template(report['details'])
+
+
+def render_reason(rule_content, reported_error_key):
+    reason_template = get_template_function('reason', rule_content, report)
+    return reason_template(report['details'])
+
+
+def render_report(report, content):
+    reported_module = get_reported_module()
+    reported_error_key = get_reported_error_key()
+
+    for rule in content:
+        if reported_module == rule['plugin']['python_module'].split('.')[-1]:
+           
+            report_result = {}
+            report_result['rule_id'] = reported_module
+            report_result['error_key'] = reported_error_key
+            report_result['resolution'] = render_resolution(rule, report)
+            report_result['reason'] = render_reason(rule, report)
+
+            return report_result
+
+
 def render_reports(request_data):
     content = request_data['content']
     reports = request_data['reports']
@@ -29,32 +76,7 @@ def render_reports(request_data):
 
     for cluster_id, cluster_data in reports['reports'].items():
         for report in cluster_data['reports']:
-            reported_module = report['rule_id'].split('|')[0]
-            reported_error_key = report['rule_id'].split('|')[1]
-
-            for rule in content:
-                if reported_module == rule['plugin']['python_module'].split('.')[-1]:
-
-                    resolution = rule['resolution']
-                    reason = rule['reason']
-
-                    error_key_content = rule['error_keys'][reported_error_key]
-                    if 'resolution' in error_key_content:
-                        resolution = error_key_content['resolution']
-                    if 'reason' in error_key_content:
-                        reason = error_key_content['reason']
-
-                    reason_template = js2py.eval_js(DoT.template(reason, DoT_settings))
-                    resolution_template = js2py.eval_js(DoT.template(resolution, DoT_settings))
-                    
-                    report_result = {}
-                    report_result['rule_id'] = reported_module
-                    report_result['error_key'] = reported_error_key
-                    report_result['resolution'] = resolution_template(report['details'])
-                    report_result['reason'] = reason_template(report['details'])
-
-                    result['reports'].setdefault(cluster_id, []).append(report_result)
-
-                    break
-
+            report_result = render_report(report, content)
+            result['reports'].setdefault(cluster_id, []).append(report_result)
+            
     return result
