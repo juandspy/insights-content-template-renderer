7f7e5ea8f34bc08a3b7f01b70ca4681b53092355
Juan Diaz Suarez <jdiazsua@redhat.com>
2023-03-27 17:32:21 +0200

    use a more specific exception

--- insights_content_template_renderer/utils.py
+++ insights_content_template_renderer/utils.py
@@ -89,7 +89,7 @@ def get_template_function(template_name, template_text, report: Report):
     template = renderer.template(escape_raw_text_for_js(template_text), DoT_settings)
     try:
         return js2py.eval_js(template)
-    except Exception as ex:
+    except js2py.internals.simplex.JsException as ex:
         log.error(f"cannot evaluate JS template")
         log.error(f"template: {template}")
         log.error(f"report: {report}")
