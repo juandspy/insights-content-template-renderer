69bbeb495b2bf71085079181eab06dcbcb343028
Juan Diaz Suarez <jdiazsua@redhat.com>
2023-06-05 12:08:18 +0200

    remove outdated test

--- insights_content_template_renderer/tests/DoT_test.py
+++ insights_content_template_renderer/tests/DoT_test.py
@@ -33,15 +33,3 @@ def test_nonletter_characters_correct_handling():
 
     text = js2py.eval_js(renderer.template(input, DoT_settings._replace(strip = False)))()
     assert text == input
-
-def test_CCXDEV_10314():
-    """Check that CCXDEV-10314 has been solved and newlines aren't an issue."""
-    INPUT = "{{?pydata.options == 1\n}}Option 1{{?? pydata.options == 2\n}}Option 2{{??\n}}Other option{{?}}:\n\nMore text"
-    want = """function anonymous(pydata) {var out='';if(pydata.options == 1){out+='Option 1';}else if(pydata.options == 2){out+='Option 2';}else{out+='Other option';}out+=':
-
-More text';return out;}"""
-    
-    settings = DEFAULT_TEMPLATE_SETTINGS
-    out = renderer.template(INPUT, settings._replace(strip = False))
-
-    assert out == want
