c418b9d375e593bd33be186d753799fe98e7986c
Jakub Drobena <jdrobena@redhat.com>
2024-09-05 11:51:35 +0200

    Setuptools library update

--- Dockerfile
+++ Dockerfile
@@ -3,17 +3,16 @@ FROM registry.access.redhat.com/ubi9-minimal:latest
 ENV VENV=/insights-content-template-renderer-venv \
     HOME=/insights-content-template-renderer
 
-RUN microdnf install --nodocs --noplugins -y python3.11
-
 WORKDIR $HOME
 
 COPY . $HOME
 
 ENV PATH="$VENV/bin:$PATH"
 
-RUN python3.11 -m venv $VENV
-RUN pip install --verbose --no-cache-dir -U pip setuptools wheel
-RUN pip install --verbose --no-cache-dir -r requirements.txt
+RUN microdnf install --nodocs --noplugins -y python3.11 && \
+    python3.11 -m venv $VENV && \
+    pip install --verbose --no-cache-dir -r requirements.txt
+
 
 # Clean up not necessary packages if useful
 RUN pip uninstall -y py #https://pypi.org/project/py/
--- pr_check.sh
+++ pr_check.sh
@@ -22,7 +22,7 @@ set -exv
 APP_NAME="ccx-data-pipeline"  # name of app-sre "application" folder this component lives in
 COMPONENT_NAME="insights-content-template-renderer"  # name of app-sre "resourceTemplate" in deploy.yaml for this component
 IMAGE="quay.io/cloudservices/insights-content-template-renderer"
-COMPONENTS="ccx-data-pipeline ccx-insights-results dvo-writer ccx-smart-proxy ccx-notification-writer ccx-notification-service ccx-notification-db-cleaner notifications-backend notifications-aggregator notifications-engine insights-content-service ccx-mock-ams ccx-upgrades-sso-mock insights-content-template-renderer"  # space-separated list of components to load
+COMPONENTS="ccx-data-pipeline ccx-insights-results dvo-writer ccx-redis ccx-smart-proxy ccx-notification-writer ccx-notification-service ccx-notification-db-cleaner notifications-backend notifications-aggregator notifications-engine insights-content-service ccx-mock-ams ccx-upgrades-sso-mock insights-content-template-renderer"  # space-separated list of components to load
 COMPONENTS_W_RESOURCES="insights-content-template-renderer"  # component to keep
 CACHE_FROM_LATEST_IMAGE="true"
 DEPLOY_FRONTENDS="false"
